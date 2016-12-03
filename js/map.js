ef78baab07c2064d60ee9dfadcebdd611de682ee
r0bot <r0bot@student.edu>
2016-12-03 18:01:53 +0100

    pozyskuje współrzędne geolokalzacji z ip-api

--- js/map.js
+++ js/map.js
@@ -1,29 +1,23 @@
 $('document').ready(function() {
+    localInit();
     
-    var map;
+    $('#map').click( function() {
+        
+    })
+});
 
 function initMap(center) {
-  map = $("#map").geomap({
-    center: center || [-71.0597732, 42.3584308],
-    zoom: 14
-  });
+    map = $("#map").geomap({
+        center: center || [-71.0597732, 42.3584308],
+        zoom: 14
+    });
 }
 
-// centruje indywidualnie dla użytkownika
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(function (p) {
-    initMap([p.coords.longitude, p.coords.latitude]);
-  }, function (error) {
-    initMap();
-  });
-} else {
-  initMap();
-}
-
-$("#map").geomap( { center: [ 0, 0 ] } );
-$('#map').click(function() {
-    var center = $("#map").geomap( "option", "center" );
-    console.log(center);})	
-
-})
-
+    // pozyskuje przybliżone współrzędne hosta użytkownika
+    // zmienna posłuży przy inicjalizacji mapy
+function localInit() {
+    var tableCoord = ""
+    $.getJSON("http://ip-api.com/json/?callback=?", function(data) {
+        initMap([data.lon, data.lat]);         
+    });
+}   
--- src/index.php
+++ src/index.php
@@ -7,24 +7,18 @@
 <head>
   <meta name="description" content="map projection" />
   <meta charset="utf-8" />
-  <meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
   <title>LoJack map</title>
   <script src="http://code.jquery.com/jquery-2.2.4.min.js"></script>
   <script src="http://code.jquerygeo.com/jquery.geo-1.0.0-rc1.min.js"></script>
   <script src="../js/map.js" type="text/javascript"></script>
-</head>
+  <link href="../CSS/map.css" rel="stylesheet" type="text/css"/>
+
+ </head>
   
 <body>
+  <div id="inne">aaa</div>
+  <div id="map" style="height: 480px;"></div>
   
-  <div id="map">
-    <div id="info">
-      <span>Using $.geo.proj to calculate geodetic &amp; projected coordinates of the center of your current map.</span>
-      <dl>
-        <dt>geodetic</dt><dd class="geodetic">[ <span class="lon"></span>, <span class="lat"></span> ]</dd>
-        <dt>projected</dt><dd class="projected">[ <span class="x"></span>, <span class="y"></span> ]</dd>
-      </dl>
-    </div>
-  </div>
   
   
 </body>
