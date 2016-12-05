07c53b44c5dfac23acb12b50a47f44d9ac5f9a51
r0bot <r0bot@student.edu>
2016-12-05 01:08:47 +0100

    dodane kolumny: col-lg-8 col-md-8

--- src/index.php
+++ src/index.php
@@ -22,29 +22,33 @@
 
  </head>
   
-<body>   
-<div id="info">
-  <h4>WSPÓŁRZĘDNE ŚRODKA EKRANU</h4>
-  <dl>
-    <dt>geodezyjny</dt><dd class="geodetic">[ <span class="lon"></span>, <span class="lat"></span> ] <span>deg</span></dd>
-    <dt>geograficzny</dt><dd class="projected">[ <span class="x"></span>, <span class="y"></span> ] <span>metry</span></dd>
-  </dl>
-  <br><br>
-  <h4>WSPÓŁRZĘDNE KURSORA</h4>
-  <dl>
-    <dt>geodezyjny</dt><dd class="geodetic_coursor">[ <span class="lon"></span>, <span class="lat"></span> ] <span>deg</span></dd>
-    <dt>geograficzny</dt><dd class="projected_coursor">[ <span class="x"></span>, <span class="y"></span> ] <span>metry</span></dd>
-  </dl>
-  <br><br>
-  <div id='request'>
-    <h4>WPROWADŹ SZUKANE PUNKTY</h4>  
-    <form name="findTheDistance" action="" method="GET">
-      <input type="text" id='x'>
-      <input type='text'id='y'>
-    </form>
-  </div>
+<body><div class="container">
+    <div class="row">
+        <div id="info" class="col-lg-4 col-md-4">
+          <h4>WSPÓŁRZĘDNE ŚRODKA EKRANU</h4>
+          <dl>
+            <dt>geodezyjny</dt><dd class="geodetic">[ <span class="lon"></span>, <span class="lat"></span> ] <span>deg</span></dd>
+            <dt>geograficzny</dt><dd class="projected">[ <span class="x"></span>, <span class="y"></span> ] <span>metry</span></dd>
+          </dl>
+          <br><br>
+          <h4>WSPÓŁRZĘDNE KURSORA</h4>
+          <dl>
+            <dt>geodezyjny</dt><dd class="geodetic_coursor">[ <span class="lon"></span>, <span class="lat"></span> ] <span>deg</span></dd>
+            <dt>geograficzny</dt><dd class="projected_coursor">[ <span class="x"></span>, <span class="y"></span> ] <span>metry</span></dd>
+          </dl>
+          <br><br>
+          <div id='request'>
+            <h4>WPROWADŹ SZUKANE PUNKTY</h4>  
+            <form name="findTheDistance" action="" method="GET">
+              <input type="text" id='x'>
+              <input type='text'id='y'>
+            </form>
+          </div>
+        </div>
+        <div id="map" class="col-lg-8 col-md-8"></div>
+    </div>
 </div>
-<div id="map"></div>
+</body>
 </body>
 </html>
   
